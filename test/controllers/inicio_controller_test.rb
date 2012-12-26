c20f22359422f845c49278796b07a8032cf63543
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2012-12-26 16:02:53 -0300

    Panel para los usuarios, bienvenida para los anónimos

---
+++ test/controllers/inicio_controller_test.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+require "./test/minitest_helper"
+
+describe InicioController do
+
+  # Este test no funciona por usar authenticated routes. Se testea con un test
+  # de integración. Ver https://github.com/plataformatec/devise/issues/1670
+  it "debe acceder a bienvenida anónimamente" do
+    # assert_routing '/', { controller: 'inicio', action: 'bienvenida' }
+  end
+
+  # Este test no funciona por usar authenticated routes. Se testea con un test
+  # de integración. Ver https://github.com/plataformatec/devise/issues/1670
+  it "debe acceder al panel si está logueado" do
+    # loguearse
+    # assert_routing '/', { controller: 'inicio', action: 'panel' }
+  end
+
+end
--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -5,10 +5,10 @@ require File.expand_path('../../config/environment', __FILE__)
 require "minitest/autorun"
 require "minitest/rails"
 
-# Uncomment if you want Capybara in accceptance/integration tests
+# Capybara in accceptance/integration tests
 require "minitest/rails/capybara"
 
-# Uncomment if you want awesome colorful output
+# Awesome colorful output
 require "minitest/pride"
 
 class MiniTest::Rails::ActiveSupport::TestCase
