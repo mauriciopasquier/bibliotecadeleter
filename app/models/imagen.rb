231aa386b0577045ba89cb4998766bf33f7e9383
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2012-12-31 19:40:18 -0300

    Thumbnails para las imágenes
    Tiene en cuenta la orientación y manda un offset para que el cuadrado siempre
    quede dentro de la ilustración.

--- app/models/imagen.rb
+++ app/models/imagen.rb
@@ -8,12 +8,13 @@ class Imagen < ActiveRecord::Base
   has_attached_file :archivo,
     { url:  "/estaticos/cartas/:style/:expansion/:carta-:numero.:extension",
       path: ":rails_root/public/cartas/:style/:expansion/:numero-:carta.:extension",
-      styles: { thumb: "" },
-      convert_options: { all: '-strip', thumb: '-crop 170x170+90+130' } }
+      styles: {
+        thumb: "170x170" },
+      convert_options: {
+        # Remueve información de esquemas de colores y EXIF
+        all: '-strip' },
+      processors: [ :cartas ]
+    }
 
   validates_attachment_presence :archivo
-
-  def extension
-    File.extname(archivo.path).delete('.')
-  end
 end
