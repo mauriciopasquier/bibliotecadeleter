e48719517dfd3ef168aa2d9d95759b64b6342766
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-01-16 17:32:01 -0300

    Uno la tarea para cargar las imágenes con db:seed
    Era necesario saber qué artista es el adecuado para cada imagen, y eso ya se
    hacía en el loop de db:seed. Además, arreglo las semillas. Las cartas con 2
    caras, *deben* indicarlo en el campo Arte, de esta forma: "artista/artista" aún
    cuando ambas caras sean dibujadas por el mismo artista. Corrijo las semillas de
    Insania para que usen "-" para separar artistas colaboradores.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 # encoding: utf-8
+require 'semillas_helper'
 # En este archivo va la carga inicial de datos. La mayoría de estos datos
 # residen en db/semillas/, en diferentes archivos y formatos. Acá se realiza la
 # carga.
 
-require 'semillas_helper'
 extend SemillasHelper
 
 # Carga las ediciones
@@ -21,15 +21,35 @@ end
 
 # Carga cada carta, versiones y artistas
 Expansion.all.each do |expansion|
+
+  unless ENV['dir']
+    raise ArgumentError, 'Necesitamos un directorio con las imágenes (dir=directorio)'
+  else
+    dir = ENV['dir']
+  end
+
+  imagenes_expansion = Dir.glob("#{Rails.root.join(dir, expansion.slug)}/*.jpg").sort
+
   cargar_csv_de(expansion.nombre.parameterize, headers: true, col_sep: ',') do |carta|
     c = Carta.find_or_create_by_nombre(atributos_de_la_carta(carta))
     v = c.versiones.create(atributos_de_la_version(carta, expansion))
 
+    imagenes_carta = imagenes_expansion.select do |i|
+      i =~ /#{v.numero_justificado}/
+    end
+
     # Divide en 2 caras los demonios, las demás cartas en 1
     carta[10].split('/').each do |cara|
+      # Creo la imagen, si es que hay
+      if imagenes_carta.any?
+        imagen = v.imagenes.create(archivo: File.open(imagenes_carta.shift))
+      end
       # Separa a los artistas cuando hay colaboraciones
       cara.split('-').each do |artista|
-        v.artistas << Artista.find_or_create_by_nombre(artista.strip)
+        a = Artista.find_or_create_by_nombre(artista.strip)
+        if imagen
+          a.ilustraciones << imagen
+        end
       end
     end
 
