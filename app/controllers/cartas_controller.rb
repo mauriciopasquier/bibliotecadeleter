bc25d4e53e2e6abd89e6ad4378888bcd7e2e76ec
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-01-24 04:31:07 -0300

    Destablizo cartas

--- app/controllers/cartas_controller.rb
+++ app/controllers/cartas_controller.rb
@@ -1,13 +1,23 @@
 # encoding: utf-8
 class CartasController < ApplicationController
+  has_scope :pagina, default: 1
 
   load_and_authorize_resource
 
   before_filter :decorar, only: [:index, :show, :edit]
 
   def index
+    # TODO Revisar si aplica lo de CanCan
+    @cartas = apply_scopes(Carta).decorate
     @titulo = 'Todas las cartas'
-    respond_with(@cartas)
+    respond_with(@cartas) do |format|
+      # TODO Esta es la mejor forma de usar ajax + kaminari? Tal vez un responder
+      format.html do
+        if request.xhr?   # solicitud ajax para la paginación
+          render :index,  layout: false
+        end
+      end
+    end
   end
 
   def show
