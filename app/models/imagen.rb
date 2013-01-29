23e2fa192b12136eaf4750d0e7fec768cd8b3ee7
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-01-29 15:23:45 -0300

    Notas

--- app/models/imagen.rb
+++ app/models/imagen.rb
@@ -6,6 +6,8 @@ class Imagen < ActiveRecord::Base
   has_one :carta, through: :version
   has_and_belongs_to_many :artistas
 
+  # TODO revisar el asunto de las imágenes default y las no disponibles, para
+  # evitar perder información (dibujante de la versión) cuando es conocida
   has_attached_file :archivo,
     { url:  ":assets/cartas/:style/:expansion/:numero-:carta:cara.:extension",
       path: ":rails_root/public/:assets/cartas/:style/:expansion/:numero-:carta:cara.:extension",
--- lib/paperclip_processors/cartas.rb
+++ lib/paperclip_processors/cartas.rb
@@ -1,6 +1,9 @@
 # enconding: utf-8
 module Paperclip
   class Cartas < Thumbnail
+    # TODO Está metiendo todo en todos los estilos
+    # Command :: convert 'arte.jpg[0]' -crop 190x190+83+120 -auto-orient -resize "190x190" -strip
+    # Command :: convert 'mini.jpg[0]' -crop 50%+83+120 -auto-orient -resize "50%" -strip
     def transformation_command
       # A menos que el estilo esté definido con "WxH#"
       unless crop?
