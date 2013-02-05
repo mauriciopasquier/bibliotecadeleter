7c8e9d80fc315e07eec3e4ebd5b5f538ba6faf37
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-02-05 03:47:05 -0300

    Habilito el cache

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,10 @@ BibliotecaDelEter::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  #config.action_controller.perform_caching = false
+
+  # Habilito el cache para pruebas
+  config.action_controller.perform_caching = true
 
   config.action_mailer do |mailer|
     # Don't care if the mailer can't send
