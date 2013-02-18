aac8d9d38a6146f15b60a2f99adda9c7bcc45e46
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-02-18 02:21:42 -0300

    Para expansiones

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,12 +17,12 @@ module ApplicationHelper
   # Título de la página para el +<head>+ por defecto, extra se determina en el
   # helper de cada controlador, dependiendo de la acción
   def titulo_de_la_aplicacion(extra = nil)
-    extra ||= titulo_de_la_accion
+    extra ||= titulo
     "#{extra.nil? ? nil : "#{extra} | "}Biblioteca Del Eter"
   end
 
   # Por defecto, no se usa nada.
-  def titulo_de_la_accion
+  def titulo
     nil
   end
 
