882a73b1af2a04ad96a9b287381fdd53b3c17c1b
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-04-29 01:28:42 -0300

    Listas públicas o privadas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130426232208) do
+ActiveRecord::Schema.define(:version => 20130429040740) do
 
   create_table "artistas", :force => true do |t|
     t.string   "nombre"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(:version => 20130426232208) do
     t.integer  "usuario_id"
     t.datetime "created_at",                    :null => false
     t.datetime "updated_at",                    :null => false
+    t.boolean  "publica",    :default => true
   end
 
   create_table "slots", :force => true do |t|
