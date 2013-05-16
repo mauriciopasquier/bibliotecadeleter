c07e03605b4dbaa6ade22433ebee4df2e5c29037
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-05-16 00:08:58 -0300

    Reservar vanity urls

---
+++ config/initializers/friendly_id.rb
@@ -0,0 +1,7 @@
+FriendlyId.defaults do |config|
+  config.use :reserved
+  # Reserve words for English and Spanish URLs
+  config.reserved_words = %w(
+    new edit nueva nuevo editar clave verificacion desbloquear carnet
+    cancelar entrar salir)
+end
