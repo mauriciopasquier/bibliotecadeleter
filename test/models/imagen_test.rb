f7206e8891860660d77484abafc8fabeae5a8198
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-09-20 02:15:22 -0300

    Al fin arreglo ese test de touch

--- test/models/imagen_test.rb
+++ test/models/imagen_test.rb
@@ -6,12 +6,25 @@ describe Imagen do
     build_stubbed(:imagen).valid?.must_equal true
   end
 
-  it "toca a los artistas a los que es asociada" do
-    artista, imagen = create(:artista), create(:imagen)
+  describe '#arte' do
+    it 'toca a los artistas asociados' do
+      artista, imagen = create(:artista, updated_at: 2.days.ago), create(:imagen)
+      vieja_key = artista.cache_key
 
-    vieja_key = artista.cache_key
-    imagen.arte = artista.nombre
+      imagen.arte = artista.nombre
 
-    vieja_key.wont_equal artista.reload.cache_key
+      vieja_key.wont_equal artista.reload.cache_key
+    end
+
+    it 'asocia varios artistas' do
+      artistas, imagen = [], create(:imagen)
+      3.times { artistas << build(:artista) }
+      # Como viene desde el usuario
+      nombres = artistas.collect(&:nombre).sort.join(', ')
+      imagen.arte = nombres
+
+      imagen.artistas.count.must_equal 3
+      imagen.arte.must_equal nombres
+    end
   end
 end
