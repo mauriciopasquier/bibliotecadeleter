814f907d9ff5574b164f6c589266f06db70dd31f
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-10-01 14:48:22 -0300

    Coleccion/Reserva privadas por default

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131001064954) do
+ActiveRecord::Schema.define(:version => 20131001164741) do
 
   create_table "artistas", :force => true do |t|
     t.string   "nombre"
