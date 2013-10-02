1d98fdd710ba26ab78d46cee697bae7c566729f3
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-10-02 02:23:48 -0300

    Dejar de crear adjuntos con los tests

--- test/factories/imagenes.rb
+++ test/factories/imagenes.rb
@@ -2,10 +2,5 @@
 FactoryGirl.define do
   factory :imagen do
     association :version, factory: :version_con_carta
-    archivo do
-      fixture_file_upload(
-        Rails.root.join('test', 'assets', 'carta.jpg'), 'image/png'
-      )
-    end
   end
 end
