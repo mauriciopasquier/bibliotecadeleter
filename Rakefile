c5f5e3ad7bc0b7d2819737cb2005809dbdc83612
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-10-11 17:02:10 -0300

    Detalles en los tests

--- Rakefile
+++ Rakefile
@@ -5,5 +5,3 @@
 require File.expand_path('../config/application', __FILE__)
 
 BibliotecaDelEter::Application.load_tasks
-
-MiniTest::Rails::Testing.default_tasks += %w{features}
--- test/factories/mazo.rb
+++ test/factories/mazo.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :mazo do
     nombre  { generate :cadena_unica }
     formato_objetivo
+    usuario
     association :principal, strategy: :build
 
     factory :mazo_con_demonios do
@@ -9,7 +10,8 @@ FactoryGirl.define do
       after(:build) do |mazo, params|
         FactoryGirl.create_list(:slot,
                                 params.cantidad,
-                                inventario: mazo)
+                                inventario: mazo,
+                                cantidad: 1)
       end
     end
   end
