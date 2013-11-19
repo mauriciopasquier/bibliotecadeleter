1206bd4ac60a93edcb9c16623b0a16140b8d7cd7
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-11-19 02:06:55 -0300

    1.6.7

--- config/application.rb
+++ config/application.rb
@@ -82,6 +82,6 @@ module BibliotecaDelEter
     config.assets.paths << Rails.root.join('app', 'assets', 'sounds')
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.6.6'
+    config.assets.version = '1.6.7'
   end
 end
