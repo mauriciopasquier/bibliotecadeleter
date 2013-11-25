a20f71f29fb2f2128cdeba497c57c4bafeed2948
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-11-25 15:46:38 -0300

    Configuración de app, entornos y gemas

--- config/initializers/friendly_id.rb
+++ config/initializers/friendly_id.rb
@@ -1,5 +1,5 @@
 FriendlyId.defaults do |config|
-  config.use :reserved
+  config.use :reserved, :finders
   # Reserve words for English and Spanish URLs
   config.reserved_words = %w(
     new edit nueva nuevo editar clave verificacion desbloquear carnet
