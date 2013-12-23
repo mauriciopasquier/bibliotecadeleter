73efb8694da31d01009f7719cf4beda523c4f954
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-12-22 21:25:48 -0300

    Show topic y posts lindos

--- config/initializers/forem.rb
+++ config/initializers/forem.rb
@@ -2,6 +2,7 @@ Forem.user_class = "Usuario"
 Forem.email_from_address = "homunculo@bibliotecadeleter.com.ar"
 # If you do not want to use gravatar for avatars then specify the method to use here:
 Forem.avatar_user_method = :avatar_decorado
+Forem.user_profile_links = true
 Forem.per_page = 20
 
 # Rails.application.config.to_prepare do
@@ -14,3 +15,6 @@ Forem.per_page = 20
 # end
 #
 # By default, these lines will use the layout located at app/views/layouts/forem.html.erb in your application.
+
+# Incluir nuestros helpers
+Forem::TopicsHelper.send :include, PaginacionHelper
