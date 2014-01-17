85b47276893ccb6f1ad539478d344331bef38e8f
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-01-17 17:33:55 -0300

    Emparejo los archivos de configuración

--- config/initializers/secret_token.dist.rb
+++ config/initializers/secret_token.dist.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
 BibliotecaDelEter::Application.config.secret_key_base = '1f96f14e5cd951fea43d48e80464deadfb057d90448d416f7be9ee982267a09fc31b93ca84a576e4a346afc3cdb482846b4e6f36eb455988a182d9d6bd152fd3'
