b2629138284781060f5f49e3d104c7bd5a8bf05f
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-09-23 19:36:51 -0300

    Arreglo un poco los tests de listas

--- test/controllers/listas_controller_test.rb
+++ test/controllers/listas_controller_test.rb
@@ -1,23 +1,23 @@
 # encoding: utf-8
-require "./test/test_helper"
+require './test/test_helper'
 
 describe ListasController do
   describe 'logueado' do
     before { @usuario = loguearse }
 
-    it "accede su lista de cartas" do
+    it 'accede su lista de cartas' do
       get :index, usuario_id: @usuario
       assert_response :success
       assigns(:listas).wont_be_nil
     end
 
-    it "intenta crear listas" do
+    it 'intenta crear listas' do
       get :new, usuario_id: @usuario
       must_respond_with :success
       assigns(:lista).wont_be_nil
     end
 
-    it "crea listas" do
+    it 'crea listas' do
       lambda do
         post :create, usuario_id: @usuario, lista: attributes_for(:lista)
       end.must_change 'Lista.count'
@@ -25,41 +25,50 @@ describe ListasController do
       must_redirect_to usuario_lista_path(@usuario, assigns(:lista))
     end
 
-    it "edita listas" do
+    it 'edita listas' do
       lista = create(:lista_con_slots, usuario: @usuario)
 
       get :edit, usuario_id: @usuario, id: lista
       must_respond_with :success
     end
 
-    it "actualiza listas" do
+    it 'actualiza listas' do
+      lista = create(:lista_con_slots, usuario: @usuario, visible: true, nombre: 'uno')
+
+      put :update, usuario_id: @usuario, id: lista, lista: { visible: false, nombre: 'otro' }
+
+      must_redirect_to usuario_lista_path(@usuario, assigns(:lista))
+
+      lista.reload
+      lista.nombre.must_equal 'otro'
+      lista.visible.must_equal false
+    end
+
+    it 'actualiza slots mediante la lista' do
       lista = create(:lista_con_slots, usuario: @usuario)
 
-      nuevo_slot = attributes_for(:slot, version_id: 99)
-      viejo_slot = {
-        id: lista.slots.first.id,
-        cantidad: lista.slots.first.cantidad.next,
-        version_id: lista.slots.first.version_id }
-
       lista.slots.count.must_equal 1
-      lista.visible.must_equal true
+      viejo_slot = lista.slots.first
+      nuevo_slot = attributes_for(:slot, version_id: 99)
 
       put :update, usuario_id: @usuario, id: lista, lista: {
-        visible: false, nombre: 'otro' }.merge(slots_attributes: {
-          '0' => nuevo_slot, viejo_slot[:id].to_s => viejo_slot }
-        )
+        slots_attributes: {
+          '0' => nuevo_slot,
+          '1' => { id: viejo_slot.id, cantidad: 999 }
+        }
+      }
 
       must_redirect_to usuario_lista_path(@usuario, assigns(:lista))
 
       lista.reload
       lista.slots.count.must_equal 2
-      lista.nombre.must_equal 'otro'
+      viejo_slot.reload.cantidad.must_equal 999
     end
 
-    it "rechaza slots vacíos" do
+    it 'rechaza slots vacíos' do
       lista = create(:lista_con_slots, usuario: @usuario)
-
       nuevo_slot = attributes_for(:slot)
+
       nuevo_slot[:version_id].must_be_nil
 
       put :update, usuario_id: @usuario, id: lista, lista: {
