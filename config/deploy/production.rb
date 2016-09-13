885a66019b61ae9f631f34e1e8615cf9b6a5ad82
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2016-09-13 00:33:46 -0300

    Actualización del VPS

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,10 @@ server 'bibliotecadeleter.com.ar', user: 'apps', roles: %w{app web db}
 set :stage_name, 'production'
 set :deploy_to, '/srv/http/bibliotecadeleter.com.ar'
 
+# Cómo reiniciar passenger
+set :passenger_restart_with_touch, true
+
 set :default_env, {
-  path: '~/.gem/ruby/2.2.0/bin:$PATH',
-  gem_home: '~/.gem/ruby/2.2.0'
+  path: '~/.gem/ruby/2.3.0/bin:$PATH',
+  gem_home: '~/.gem/ruby/2.3.0'
 }
